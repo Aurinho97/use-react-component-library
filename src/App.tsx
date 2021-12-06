df3d29c74c7cf9f50baa694bb9ecde9d9c64f46e
aurimas1997 <aurimas.tunaitis@interactio.io>
2021-12-06 19:47:36 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Button } from "@Aurinho97/react-component-library";
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <Button label="Primary" variant="primary"/>
+            <Button label="Secondary" variant="secondary"/>
+        </div>
+    );
 }
 
 export default App;
