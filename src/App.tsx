98f1fac8d8333a365649b43d1267b707e36209a4
aurimas1997 <aurimas.tunaitis@interactio.io>
2021-12-07 14:03:48 +0200

    Use global token from library in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import { Button } from "@Aurinho97/react-component-library";
+import styles from './App.module.scss';
 
 const App: React.FC = () => {
     return (
         <div>
             <Button label="Primary" variant="primary"/>
             <Button label="Secondary" variant="secondary"/>
+            <div className={styles.div}>Hello World!</div>
         </div>
     );
 }
